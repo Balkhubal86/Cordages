d1e7d319b2c9c977f91dea8129496baef8cb69cb
Baptiste <baptiste86@hotmail.com>
2024-05-27 17:19:15 +0200

    last push 27/05

--- site/Views/header.php
+++ site/Views/header.php
@@ -33,8 +33,18 @@
                             <li><a class="dropdown-item" href="index.php?view=history">Notre Histoire</a></li>
                             <li><a class="dropdown-item" href="index.php?view=value">Nos Valeurs</a></li>
                             <li><a class="dropdown-item" href="index.php?view=mission">Nos Missions</a></li>
-                            <li><a class="dropdown-item" href="index.php?view=team">Notre Equipe</a></li>
-                            <li><a class="dropdown-item" href="index.php?view=partner">Nos Partenaire</a></li>
+                            <li>
+                                <a class="dropdown-item" href="index.php?view=team">Notre Equipe
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-lg" viewBox="0 0 16 16" style="color: red;">
+                                    <path d="M7.005 3.1a1 1 0 1 1 1.99 0l-.388 6.35a.61.61 0 0 1-1.214 0zM7 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0"/>
+                                </svg></a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="index.php?view=partner">Nos Partenaire
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-lg" viewBox="0 0 16 16" style="color: red;">
+                                    <path d="M7.005 3.1a1 1 0 1 1 1.99 0l-.388 6.35a.61.61 0 0 1-1.214 0zM7 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0"/>
+                                </svg></a>
+                            </li>
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
