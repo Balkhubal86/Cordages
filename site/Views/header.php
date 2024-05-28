02d545d1a818529eee662aad363004bb4ac5857b
Baptiste <baptiste86@hotmail.com>
2024-05-28 23:53:39 +0200

    good night

--- site/Views/header.php
+++ site/Views/header.php
@@ -7,6 +7,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+
     <link rel="stylesheet" href="public/assets/css/style.css">
 </head>
 <body>
@@ -77,7 +78,7 @@
 
                     <!-- Page Connexion -->
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php?view=connexion" style="color : black">Connexion</a>
+                        <a class="nav-link" href="index.php?view=connexion&action=display" style="color : black">Connexion</a>
                     </li>
                 </ul>
                 
